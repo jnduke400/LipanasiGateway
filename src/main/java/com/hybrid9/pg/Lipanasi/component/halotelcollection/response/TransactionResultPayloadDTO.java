e66d085f8fe40061e64673b02e7e44084eace0f6
jnduke400 <ndukep@gmail.com>
2026-03-03 10:03:50 +0300

    -fix: fixing package of TransactionResultPayloadDTO class

--- src/main/java/com/hybrid9/pg/Lipanasi/component/halotelcollection/response/TransactionResultPayloadDTO.java
+++ src/main/java/com/hybrid9/pg/Lipanasi/component/halotelcollection/response/TransactionResultPayloadDTO.java
@@ -1,6 +1,6 @@
 package com.hybrid9.pg.Lipanasi.component.halotelcollection.response;
 
-import com.gtl.pg.scoop.component.halotelcollection.request.CommonHeaderDTO;
+import com.hybrid9.pg.Lipanasi.component.halotelcollection.request.CommonHeaderDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
